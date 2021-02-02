27e06f2de641c136c30ca323376644d35e7ac03c
Mac <mac@MacBook-Pro-de-Mac.local>
2021-02-02 10:30:42 -0400

    login parte 1

---
+++ lib/config/constantes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// Todas as Cores do Aplicativo
+const corPrimaria = Color(0xFFdc3545);
+const corSecundaria = Color(0xFF151710);
+
+const coPrimariaGradiente = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: [Color(0xFFdc3545), Color(0xFFb50011)]);
+// Final das Cores do Aplicativo
+
+// Tamanho dos Widget
+
+// Fim Tamanho dos Objetos
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:olimpia_food/config/constantes.dart';
 import 'package:olimpia_food/views/pages/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: corPrimaria,
+      accentColor: corPrimaria,
+      cursorColor: corPrimaria,
+    ),
     home: Login(),
   ));
 }
